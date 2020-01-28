2d722b19bf7bc47e779f5a243a19a05f9deda5bf
dallinbledsoe <dallinbledsoe@gmail.com>
2020-01-27 18:15:09 -0700

    added shop hats and shirts comps

---
+++ src/components/front-page/shop-hats.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react"
+
+export default class ShopHats extends Component {
+
+    render() {
+return (
+ <div className="module-wrapper">
+     <div className="module">
+         <src img="">
+             <button>
+                 
+             </button>
+         </src>
+     </div>
+ </div>
+)
+}
+}
