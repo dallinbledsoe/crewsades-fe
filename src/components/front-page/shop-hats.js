ceebbc3e4f7b766ea312b7f0efd50b818db438c1
dallinbledsoe <dallinbledsoe@gmail.com>
2020-02-04 17:40:40 -0700

    added modules on frontpage

--- src/components/front-page/shop-hats.js
+++ src/components/front-page/shop-hats.js
@@ -4,15 +4,14 @@ export default class ShopHats extends Component {
 
     render() {
 return (
- <div className="module-wrapper">
-     <div className="module">
-         <src img="">
-             <button>
-                 
-             </button>
-         </src>
-     </div>
- </div>
+    <div className="module-wrapper">
+        <Link to="/headwear">
+            <img src="https://res.cloudinary.com/de1fkeds5/image/upload/v1580863047/shop-hats_mryuvu.jpg">
+                Shop Hats
+            </img>
+        </Link>
+
+    </div>
 )
 }
 }
--- src/components/front-page/shop-shirts.js
+++ src/components/front-page/shop-shirts.js
@@ -3,5 +3,12 @@ import React from "react"
 export default function();
 
 return (
-    <div></div>
+    <div className="module-wrapper">
+        <Link to="/shirts">
+            <img src="https://res.cloudinary.com/de1fkeds5/image/upload/v1579717302/_MG_0795_fr3ran.jpg">
+                Shop Shirts
+            </img>
+        </Link>
+
+    </div>
 )
